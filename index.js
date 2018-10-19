7a62472e4a0b8e433f2cfef24ddbd7158f25da10
Eric Boutueil <e.boutueil@gmail.com>
2018-10-19 11:04:10 +0200

    index.js wip

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
+// if (typeof web3 !== 'undefined') {
+//   const web3 = new Web3(web3.currentProvider);
+// } else {
+  const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// }
 
 // web3.eth.defaultAccount = web3.eth.getAccounts().then(result => {
 //   let firstAccount=result[0]
 // });
 
-abi = [
+const abi = [
   {
     "constant": false,
     "inputs": [
@@ -112,28 +112,41 @@ abi = [
   }
 ]
 
-contractAddress = '0x08970fed061e7747cd9a38d680a601510cb659fb'
+const contractAddress = '0x08970fed061e7747cd9a38d680a601510cb659fb'
 
-VotingForEricContract = new web3.eth.contract(abi);
-contractInstance = VotingForEricContract.at(contractAddress);
+const VotingForEricContract = new web3.eth.contract(abi);
+const contractInstance = VotingForEricContract.at(contractAddress);
 
 console.log(contractInstance)
 
-candidates = {"Eric B": "candidate-1", "E B": "candidate-2", "E Boutueil": "candidate-3"}
+const candidates = {"Eric B": "candidate-1", "E B": "candidate-2", "E Boutueil": "candidate-3"}
 
-function voteForCandidate() {
+// function voteForCandidate() {
+//   candidateName = $("#candidate").val();
+//   contractInstance.voteForCandidate(candidateName, {from: '0x147e02dA5E0691180A3b2C0f2C4381D1fF5F4205'}, function() {
+//     let div_id = candidates[candidateName];
+//     $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+//   });
+// }
+
+let candidateName
+
+function totalVotesFor(candidateName) {
+  let div_id = candidates[candidateName];
+  $("#candidate-" + div_id).html(contractInstance.methods.totalVotesFor(candidateName).send({}).toString());
+}
+
+function voteForCandidate(totalVotesFor) {
   candidateName = $("#candidate").val();
-  contractInstance.voteForCandidate(candidateName, {from: '0x147e02dA5E0691180A3b2C0f2C4381D1fF5F4205'}, function() {
-    let div_id = candidates[candidateName];
-    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
-  });
+  contractInstance.methods.voteForCandidate(candidateName).send({});
+  totalVotesFor(candidateName);
 }
 
 $(document).ready(function() {
   Erics = Object.keys(candidates);
   for (var i = 0; i < Erics.length; i++) {
     let name = Erics[i];
-    let val = contractInstance.totalVotesFor.call(name).toString()
-    $("#" + candidates[name]).html(val);
+    let val = contractInstance.methods.totalVotesFor(name).send({}).toString()
+    $("#candidate-" + candidates[name]).html(val);
   }
 });
